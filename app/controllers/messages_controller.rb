653a213e3163dabd64a9968d85677be449a52588
yuya <yuya.skd@gmail.com>
2016-10-12 15:48:50 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,10 @@
 class MessagesController < ApplicationController
+  
+  
+  
+  
+  
+  
   def index
     @message = Message.new
     # Messageを全て取得する。
@@ -7,8 +13,14 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
   end
 
   private
