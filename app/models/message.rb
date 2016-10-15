a1e08686f3c6640a38a570b377b71a0b1a8c8e34
yuya <yuya.skd@gmail.com>
2016-10-15 12:17:39 +0000

    first commit

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+  validates :age, numericality: true
 end
